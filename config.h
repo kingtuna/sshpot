fd9dafe57d073458bd8af48ac560b0fa5396c9ed
Pete Morris <pete.morris@gmail.com>
2011-11-20 11:47:39 -0500

    Basic implementation of MAX CONNECTIONS. Causes clients beyond MAX CONNECTIONS to hang.

--- config.h
+++ config.h
@@ -1,3 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
 #define LISTENADDRESS   "localhost"
 #define PORT            "22000"
+#define MAXCONNECTIONS  1
 #define DEBUG           1
+
+#endif
--- main.c
+++ main.c
@@ -6,18 +6,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 #define KEYS_FOLDER "./"
+
+/* Keep track of the number of concurrent connections. */
+static int total_connections = 0;
+
+static int cleanup(void) {
+    int status;
+    int pid;
+    pid_t wait3(int *statusp, int options, struct rusage *rusage);
+
+    while ((pid=wait3(&status, WNOHANG, NULL)) > 0) {
+        if (DEBUG) { printf("process %d reaped\n", pid); }
+        total_connections--;
+        printf("*** Decrementing total_connections: %d ***\n", total_connections);
+    }
+
+    /* Re-install myself for the next child */
+    signal(SIGCHLD, (void (*)())cleanup);
+
+    return 0;
+}
 
 
 int main(int argc, char **argv) {
     ssh_session session;
     ssh_bind sshbind;
+    int child_ret = 0;
 
-    sshbind=ssh_bind_new();
+    /* Install the handler to cleanup after children. */
+    signal(SIGCHLD, (void (*)())cleanup);
+
     session=ssh_new();
-
+    sshbind=ssh_bind_new();
     ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDADDR, LISTENADDRESS);
     ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_BINDPORT_STR, PORT);
     ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_DSAKEY, KEYS_FOLDER "sshpot.dsa.key");
@@ -32,13 +56,13 @@ int main(int argc, char **argv) {
 
     //Loop here.
     while (1) {
-    
         /* Wait for a connection. */
-        if (DEBUG) { printf("Accepted a connection.\n"); }
         if (ssh_bind_accept(sshbind, session) == SSH_ERROR) {
-        printf("error accepting a connection : %s\n",ssh_get_error(sshbind));
-        return 1;
+            printf("Error accepting a connection: %s\n",ssh_get_error(sshbind));
+            return -1;
         }
+        if (DEBUG) { printf("Accepted a connection.\n"); }
+        total_connections++;
 
         //Fork here.
         switch (fork())  {
@@ -47,7 +71,16 @@ int main(int argc, char **argv) {
                 exit(-1);
 
             case 0:
-                exit(handle_auth(session));
+                printf("*** Total connections = %d ***\n", total_connections);
+                if (total_connections > MAXCONNECTIONS) {
+                    printf("Too many connections!\n");
+                    /* This causes the client to hang. */
+                    ssh_disconnect(session);
+                }
+                else {
+                    child_ret = handle_auth(session);
+                }
+                exit(child_ret);
 
             default:
                 break;
