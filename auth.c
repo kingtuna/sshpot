69f77ca412419b95199e6b8b31bc2b820e90c306
Pete Morris <pete.morris@gmail.com>
2011-11-21 22:18:24 -0500

    Log the time of the client request.

--- auth.c
+++ auth.c
@@ -4,14 +4,31 @@
 #include <libssh/server.h>
 
 #include <stdio.h>
+#include <time.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define MAXBUF 100
+
+/* Stores the current UTC time in buf. */
+static char *get_utc(char *buf) {
+    time_t t;
+    struct tm *utc;
+
+    t = time(NULL);
+    utc = gmtime(&t);
+    if (strftime(buf, MAXBUF, "%Y%m%d %H:%M:%S", utc) == 0) {
+        buf = NULL;
+    }
+    return buf;
+}
+
 
 /* Stores the client's IP address in buf. */
-static char *get_client_ip(ssh_session session, char *buf, unsigned int len) {
+static char *get_client_ip(ssh_session session, char *buf) {
     struct sockaddr_storage addr;
     struct sockaddr_in *sock;
+    unsigned int len = MAXBUF;
     int s;
 
     s = ssh_get_fd(session);
@@ -27,7 +44,8 @@ static char *get_client_ip(ssh_session session, char *buf, unsigned int len) {
 /* Logs password auth attempts. Always replies with SSH_MESSAGE_USERAUTH_FAILURE. */
 int handle_auth(ssh_session session) {
     ssh_message message;
-    char addr[INET_ADDRSTRLEN];
+    char c_addr[MAXBUF];
+    char c_time[MAXBUF];
 
     /* Perform key exchange. */
     if (ssh_handle_key_exchange(session)) {
@@ -44,8 +62,11 @@ int handle_auth(ssh_session session) {
 
         /* Log the authentication request and disconnect. */
         if (ssh_message_subtype(message) == SSH_AUTH_METHOD_PASSWORD) {
-            get_client_ip(session, addr, sizeof addr);
-            printf("%s %s %s\n", get_client_ip(session, addr, sizeof addr), 
+            if (get_utc(c_time) == NULL) {
+                fprintf(stderr, "Error getting time.\n");
+                return -1;
+            }
+            printf("%s %s %s %s\n", c_time, get_client_ip(session, c_addr), 
                     ssh_message_auth_user(message), ssh_message_auth_password(message));
         }
 
