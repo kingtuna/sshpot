4cde589fbb8888ff22054b9f1f2d46378ba49636
Pete Morris <pete.morris@gmail.com>
2012-09-01 07:21:47 -0400

    Store all connection info in a struct

--- auth.c
+++ auth.c
@@ -1,3 +1,4 @@
+#include "auth.h"
 #include "config.h"
 
 #include <libssh/libssh.h>
@@ -8,61 +9,69 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define MAXBUF 100
 
 
-/* Write interesting information about a connection attempt to  LOGFILE. */
-static int log_attempt(char *time, char *ip, char *user, char *pass) {
+/* Stores the current UTC time. Returns 0 on error. */
+static int get_utc(struct connection *c) {
+    time_t t;
+    t = time(NULL);
+    return strftime(c->con_time, MAXBUF, "%Y-%m-%d %H:%M:%S", gmtime(&t));
+}
+
+
+/* Stores the client's IP address in the connection sruct. */
+static int *get_client_ip(struct connection * c) {
+    struct sockaddr_storage tmp;
+    struct sockaddr_in *sock;
+    unsigned int len = MAXBUF;
+
+    getpeername(ssh_get_fd(c->session), (struct sockaddr*)&tmp, &len);
+    sock = (struct sockaddr_in *)&tmp;
+    inet_ntop(AF_INET, &sock->sin_addr, c->client_ip, len);
+
+    return 0;
+}
+
+
+/* Write interesting information about a connection attempt to  LOGFILE. 
+ * Returns -1 on error. */
+static int log_attempt(struct connection *c) { //char *time, char *ip, char *user, char *pass) {
     FILE *f;
     int r;
 
     if ((f = fopen(LOGFILE, "a+")) == NULL) {
+        fprintf(stderr, "Unable to open %s\n", LOGFILE);
         return -1;
     }
 
-    if (DEBUG) { printf("%s %s %s %s\n", time, ip, user, pass); }
-    r = fprintf(f, "%s %s %s %s\n", time, ip, user, pass);
+    if (get_utc(c) <= 0) {
+        fprintf(stderr, "Error getting time\n");
+        return -1;
+    }
+
+    if (get_client_ip(c) < 0) {
+        fprintf(stderr, "Error getting client ip\n");
+        return -1;
+    }
+
+    c->user = ssh_message_auth_user(c->message);
+    c->pass = ssh_message_auth_password(c->message);
+
+    if (DEBUG) { printf("%s %s %s %s\n", c->con_time, c->client_ip, c->user, c->pass); }
+    r = fprintf(f, "%s %s %s %s\n", c->con_time, c->client_ip, c->user, c->pass);
     fclose(f);
     return r;
 }
 
 
-/* Stores the current UTC time in buf. */
-static char *get_utc(char *buf) {
-    time_t t;
-
-    t = time(NULL);
-    if (strftime(buf, MAXBUF, "%Y-%m-%d %H:%M:%S", gmtime(&t)) == 0) {
-        buf = NULL;
-    }
-
-    return buf;
-}
-
-
-/* Stores the client's IP address in buf. */
-static char *get_client_ip(ssh_session session, char *buf) {
-    struct sockaddr_storage tmp;
-    struct sockaddr_in *sock;
-    unsigned int len = MAXBUF;
-
-    getpeername(ssh_get_fd(session), (struct sockaddr*)&tmp, &len);
-    sock = (struct sockaddr_in *)&tmp;
-    inet_ntop(AF_INET, &sock->sin_addr, buf, len);
-
-    return buf;
-}
-
-
 /* Logs password auth attempts. Always replies with SSH_MESSAGE_USERAUTH_FAILURE. */
 int handle_auth(ssh_session session) {
-    ssh_message message;
-    char c_addr[MAXBUF];
-    char c_time[MAXBUF];
+    struct connection con;
+    con.session = session;
 
     /* Perform key exchange. */
-    if (ssh_handle_key_exchange(session)) {
-        fprintf(stderr, "Error exchanging keys: `%s'.\n", ssh_get_error(session));
+    if (ssh_handle_key_exchange(con.session)) {
+        fprintf(stderr, "Error exchanging keys: `%s'.\n", ssh_get_error(con.session));
         return -1;
     }
     if (DEBUG) { printf("Successful key exchange.\n"); }
@@ -70,24 +79,21 @@ int handle_auth(ssh_session session) {
     /* Wait for a message, which should be an authentication attempt. Send the default
      * reply if it isn't. Log the attempt and quit. */
     while (1) {
-        if ((message = ssh_message_get(session)) == NULL) {
+        if ((con.message = ssh_message_get(con.session)) == NULL) {
             break;
         }
 
         /* Log the authentication request and disconnect. */
-        if (ssh_message_subtype(message) == SSH_AUTH_METHOD_PASSWORD) {
-            if (get_utc(c_time) != NULL) {
-                log_attempt(c_time, get_client_ip(session, c_addr),
-                    ssh_message_auth_user(message), ssh_message_auth_password(message));
-            }
-            else {
-                fprintf(stderr, "Error getting time.\n");
-            }
+        if (ssh_message_subtype(con.message) == SSH_AUTH_METHOD_PASSWORD) {
+                log_attempt(&con);
+        }
+        else {
+            if (DEBUG) { fprintf(stderr, "Not a password authentication attempt.\n"); }
         }
 
         /* Send the default message regardless of the request type. */
-        ssh_message_reply_default(message);
-        ssh_message_free(message);
+        ssh_message_reply_default(con.message);
+        ssh_message_free(con.message);
     }
 
     if (DEBUG) { printf("Exiting child.\n"); }
--- auth.h
+++ auth.h
@@ -3,6 +3,17 @@
 
 #include <libssh/libssh.h>
 
+#define MAXBUF 100
+
+struct connection {
+    ssh_session session;
+    ssh_message message;
+    char client_ip[MAXBUF];
+    char con_time[MAXBUF];
+    char *user;
+    char *pass;
+};
+
 int handle_auth(ssh_session session);
 
 #endif
