3ccea86c674be7fdf6cb80a909f9b2f06e435b82
kingtuna <secretpackets@gmail.com>
2014-04-21 00:50:28 -0700

    Update config.h

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 #define LISTENADDRESS   "0.0.0.0"
 #define DEFAULTPORT     22
 #define RSA_KEYFILE     "./sshpot.rsa.key"
-#define LOGFILE         "sshpot_auth.log"
+#define LOGFILE         "/var/log/sshpot_auth.log"
 #define DEBUG           0
 
 #endif
