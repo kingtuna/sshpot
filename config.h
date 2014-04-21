cbdf7c1e30b9aa99e9ae5c9ace8acab9fc4cb88b
Terrence Gareau <secretpackets@gmail.com>
2014-04-21 04:05:03 -0400

    using default ubuntu 12.04lts key

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 #define LISTENADDRESS   "0.0.0.0"
 #define DEFAULTPORT     22
-#define RSA_KEYFILE     "./sshpot.rsa.key"
+#define RSA_KEYFILE     "/etc/ssh/ssh_host_rsa_key"
 #define LOGFILE         "/var/log/sshpot_auth.log"
 #define DEBUG           0
 
